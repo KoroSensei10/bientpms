48eba56e1d45df28e027b84215be30844b471835
Sufyan AIT MANSSOUR <sufyan.ait-manssour@etudiant.univ-reims.fr>
2021-11-28 13:14:15 +0100

    Création de Home.vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,41 +3,47 @@ import Accueil from '../views/Accueil.vue'
 import Login from '../views/Login.vue'
 import Profile from '../views/Profile.vue'
 import Register from '../views/Register.vue'
+import Home from '../views/Home.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Accueil',
-    component: Accueil
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  },
-  {
-    path: '/profile',
-    name: 'Profile',
-    component: Profile
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Accueil',
+        component: Accueil
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: Profile
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ui icon input" style="width: 100%">
+    <input type="text" placeholder="Search..." />
+    <i class="search icon"></i>
+    
+  </div>
+  <div
+      class="card ui fluid"
+      v-for="product in products"
+      :key="product.id"
+      style="margin: 0"
+    >
+  </div>
+</template>
+
+<script>
+const home = Vue.createApp({})
+home.component('search-bar',{
+  data(){
+
+  }
+}
+)
+
+</script>
+
