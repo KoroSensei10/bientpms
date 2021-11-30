a82ec22e60c91b04a4b9427af530b9b7f8ff9eb8
KoroSensei10 <50498039+KoroSensei10@users.noreply.github.com>
2021-11-30 04:03:05 +0100

    Filtre pour les activités actives

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -3,48 +3,25 @@
     tu mets genre un attribut de l'objet this.activityInfo
     par exemple pour le titre de la carte tu mets {{ this.activityInfo.title }}
     pour avoir toutes les infos de l'activité regarde sur l'api de Liam http://157.90.237.150/docs#/activity/read_activities_api_v1_activity_get (dans l'onglet réponse)-->
-  <div class="activite .d-flex">
-    <div
-      class=".d-flex .container-fluid .bg-trasparent .my-4 .p-3"
-      style="position: relative"
-    >
-      <div class=".row">
-        <div class="card .h-100 .shadow-sm">
-          <img
-            :src="this.activityInfo.image"
-            class="card-img-top"
-            alt="image du sport"
-          />
-          <div class="card-body">
-            <div class="clearfix mb-3">
-              <span v-for="level in this.activityInfo.levels" v-bind:key="level.id" class="">
-                {{level.level}}
-              </span>
-            </div>
-            <h5 class="card-title">{{ this.activityInfo.title }}</h5>
-            <p class="card-date">{{ this.activityInfo.event_date }}</p>
-            <p class="card-cp">{{ this.activityInfo.postcode }} / {{ this.activityInfo.address }}</p>
-            <p class="card-sport">{{ this.activityInfo.sport.name }}</p>
-
-            <div v-if="this.participant">
-              <div class=".text-center my-4">
-                <a href="#" class=".btn btn-info">Rejoindre</a>
-              </div>
-            </div>
-            <div v-else>
-              <div class="createur" style="align-item">
-                <div class="text-center my-4">
-                  <a href="#" class=".btn btn-primary">Modifier</a>
-                </div>
-                <div class="text-center my-4">
-                  <a href="#" class=".btn btn-danger">Supprimer</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div class="card text-center m-3 flex-grow-1">
+    <div class="card-header">{{ this.activityInfo.title }}</div>
+    <div class="card-body">
+      <h5 class="card-title">{{ this.activityInfo.sport.name }}</h5>
+      <p class="card-text">
+        {{ this.activityInfo.description }}
+      </p>
+      <p class="card-text">
+        <strong>Se déroule le :</strong> {{ this.activityInfo.event_date }}
+      </p>
+      <p class="card-text">
+        <strong>Niveau : </strong>
+        <span class="badge rounded-pill bg-success" v-for="level in this.activityInfo.levels" v-bind:key="level.id"> {{level.level}}</span>
+      </p>
+      <a v-if="participant" href="#" class="btn btn-primary float-center">Participer</a>
+      <a v-if="!participant" href="#" class="btn btn-primary float-start">Modifier</a>
+      <a v-if="!participant" href="#" class="btn btn-danger float-end">Supprimer</a>
     </div>
+    <div class="card-footer text-muted">{{ this.activityInfo.postcode }} / {{ this.activityInfo.address }}</div>
   </div>
 </template>
 
@@ -58,23 +35,5 @@ export default {
 };
 </script>
 <style>
-body {
-  height: 100%;
-  margin: 0;
-  display: flex;
-  flex-direction: column;
-  border-style: none;
-  background-color: #fff;
-}
 
-.card {
-  font-weight: bold;
-  font-size: 15px;
-  border: 1px solid black;
-  margin-left: 15px;
-}
-.img {
-  width: 50px;
-  height: 50px;
-}
 </style>
--- src/views/UserActivities.vue
+++ src/views/UserActivities.vue
@@ -1,7 +1,15 @@
 <template>
-    <div class="d-flex flex-sm-column">
+    <div class="d-flex flex-column">
         <h1>Page activités de l'user</h1>
-        <div class="d-flex flex-sm-column" v-if="activities">
+        <div class="d-flex justify-content-center align-items-center">
+            <h4>Filters : </h4>
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" v-on:change="getActivities()"
+                    role="switch" id="flexSwitchCheckChecked" v-model="this.activitiesRequestInfo.active">
+                <label class="form-check-label" for="flexSwitchCheckChecked">Activités actives</label>
+            </div>
+        </div>
+        <div class="d-flex flex-column flex-sm-row flex-wrap" v-if="activities">
             <Activity v-for="activity in activities" v-bind:key="activity.id" :activity-info="activity"/>
         </div>
         <div v-if="this.message" class="alert alert-danger" role="alert">
@@ -20,8 +28,7 @@ export default {
     data() {
         return {
             activitiesRequestInfo : {
-                active: false,
-                postcode: 51100,
+                active: true,
                 offset: 0,
                 limit: 10,
             },
@@ -31,10 +38,9 @@ export default {
         }
     },
     methods: {
-        getActivities(requestInfo) {
-            GestionActivities.getActiveUserActivities(requestInfo).then((data) => {
+        getActivities() {
+            GestionActivities.getActiveUserActivities(this.activitiesRequestInfo).then((data) => {
             this.loading = false;
-            console.log(data)
             this.activities = data;
         }).catch((error) => {
             this.loading = false;
@@ -53,7 +59,7 @@ export default {
     },
     beforeMount() {
         //récup les activités de l'user et les mets sous forme de card
-        this.getActivities(this.activitiesRequestInfo)
+        this.getActivities()
     }
 }
 </script>
