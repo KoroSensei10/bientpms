66767771621d072b8da823b3b66f6a6aa979a0ea
KoroSensei10 <50498039+KoroSensei10@users.noreply.github.com>
2021-11-30 04:45:26 +0100

    Filtre sur la page home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,38 @@
 <template>
   <!-- barre de recherche -->
-  <div
-    class="
-      d-flex
-      text-center
-      justify-content-center
-      text-white
-      p-1
-      size-title
-      color
-    "
-  >
-    Accueil
+  <div class="d-flex flex-column">
+    <div class="d-flex text-center justify-content-center text-white p-1 size-title color">
+      Home
+    </div>
+    <!-- <div class=".d-flex text-center justify-content-center">
+      <input
+        type="search"
+        class="form-control rounded"
+        placeholder="Rechercher un joueur ou une activité..."
+      />
+    </div> -->
+    <div class="d-flex flex-column flex-sm-row justify-content-center align-items-center">
+      <h4>Filters : </h4>
+      <div class="form-group flex-grow-1 d-flex mb-3 justify-content-center align-items-center">
+        <label class="form-label flex-grow-1" for="">Code Postal</label>
+        <input class="form-control flex-grow-0" type="text" v-model="this.activitiesRequestInfo.postcode">
+      </div>
+      <div class="d-flex mb-3 flex-grow-1 justify-content-center align-items-center">
+        <label class="form-label">Sport</label>
+        <select class="form-select" aria-label="Select pour le sport" v-model="this.activitiesRequestInfo.sport_id">
+          <option v-for="sport in this.sports" v-bind:key="sport.id" :value="sport.id">
+            {{ sport.name }}
+          </option>
+        </select>
+      </div>
+      <button @click="getActivities()" class="btn btn-primary m-3">Chercher</button>
+      <button @click="resetSearch()" class="btn btn-danger">Reset</button>
+    </div>
+    <div class="d-flex flex-column flex-sm-row flex-wrap" v-if="activities">
+      <Activity v-for="activity in activities" v-bind:key="activity.id" :activity-info="activity" :participant="true"/>
+    </div>
   </div>
-  <div class=".d-flex text-center justify-content-center">
-    <input
-      type="search"
-      class="form-control rounded"
-      placeholder="Rechercher un joueur ou une activité..."
-    />
-  </div>
-
-  <div class="container flex-row d-flex flex-row justify-content-center">
+  <!-- <div class="container flex-row d-flex flex-row justify-content-center">
     <div class=".d-flex .flex-fill card-group justify-content-center">
       <div class=".d-flex justify-content-center m-3">
         <div class="card border-dark" style="width: 20rem; margin-left: 3px">
@@ -158,13 +169,7 @@
         :participant="true"
       />
     </div>
-    <div
-      class="card ui fluid"
-      v-for="product in products"
-      :key="product.id"
-      style="margin: 0"
-    ></div>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -178,22 +183,20 @@ export default {
   data() {
     return {
       activitiesRequestInfo: {
-        active: false,
-        sport_id: null,
-        postcode: null,
+        postcode: "51100",
         offset: 0,
         limit: 10,
-        levels: [],
       },
       activities: null,
       message: null,
       loading: false,
+      sports: null,
+      levels: ["débutant", "amateur", "intermédiaire", "confirmé", "expert"],
     };
   },
   methods: {
-    getActivities(requestInfo) {
-      GestionActivities.getActiveUserActivities(requestInfo)
-        .then((data) => {
+    getActivities() {
+      GestionActivities.getActivitiesBySearch(this.activitiesRequestInfo).then((data) => {
           this.loading = false;
           this.activities = data;
         })
@@ -201,6 +204,10 @@ export default {
           this.loading = false;
           this.message = error;
         });
+    },
+    resetSearch() {
+      this.activitiesRequestInfo = {postcode: "51100",offset: 0, limit: 10,};
+      this.getActivities();
     },
     logout() {
       this.$store.dispatch("logout");
@@ -214,7 +221,15 @@ export default {
   },
   beforeMount() {
     //récup les activités de l'user et les mets sous forme de card
-    this.getActivities(this.activitiesRequestInfo);
+    this.getActivities();
+
+    GestionActivities.getSports()
+      .then((sports) => {
+        this.sports = sports;
+      })
+      .catch((error) => {
+        this.message = error;
+    });
   },
 };
 </script>
@@ -238,147 +253,3 @@ export default {
   font-size: 20px;
 }
 </style>
-<!--
-                               des tests 
-<div
-    class="card ui fluid"
-    v-for="product in products"
-    :key="product.id"
-    style="margin: 0"
-  ></div>
-   <div class="activite .d-flex">
-    <div
-      class=".d-flex .container-fluid .bg-trasparent .my-4 .p-3"
-      style="position: relative"
-    >
-      <div class=".row">
-        <div class="card .h-100 .shadow-sm">
-          <img
-            src="/images/foot.jpg"
-            class="card-img-top"
-            alt="image du sport"
-          />
-          <div class="card-body">
-            <div class="clearfix mb-3">
-              <span class="float-start badge .rounded-pill .bg-primary">{{
-                debutant
-              }}</span>
-            </div>
-            <h5 class="card-title">Titre</h5>
-            <p class="card-date">11/12/2013</p>
-            <p class="card-cp">51100</p>
-            <p class="card-sport">football</p>
-
-            <div v-if="participant">
-              <div class=".text-center my-4">
-                <a href="" class=".btn btn-info">Rejoindre</a>
-              </div>
-            </div>
-            <div v-else>
-              <div class="createur" style="align-item">
-                <div class="text-center my-4">
-                  <a href="" class=".btn btn-primary">Modifier</a>
-                </div>
-                <div class="text-center my-4">
-                  <a href="" class=".btn btn-danger">Supprimer</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-  <div class="container .d-flex  justify-content-center ">
-    
-    <div class=".d-flex .flex-fill card-group justify-content-center">
-<div class=".d-flex justify-content-center m-3 "> 
-    <div class="card border-dark" style="width: 20rem;">
-        <img src="/images/foot.jpg"  class="card-img-top w-100" alt="imageSport">
-        <div class="card-body">
-            <div class="clearfix mb-3">
-              <span class="float-start badge rounded-pill bg-primary w-20">
-                debutant
-              </span>
-            </div>
-            <h5 class="card-title">Tournoi de Football</h5>
-            <p class="card-subtitle">Venez passez un bon temps avec des amateurs de foot comme vous ! </p>
-            <p class="card-text">14/12/2021</p>
-            <p class="card-text">51100</p>
-            <p class="card-text">Football</p>
-            
-        </div>
-        </div>
-    </div>  
-    <div class=".d-flex justify-content-center m-3 "> 
-        <div class="card border-dark " style="width: 20rem;">
-            <img src="/images/foot.jpg"  class="card-img-top w-100" alt="imageSport">
-            <div class="card-body">
-                <h5 class="card-title">Tournoi de Tennis</h5>
-                <p class="card-subtitle">Procedente igitur mox tempore cum adventicium nihil inveniretur, relicta ora maritima . </p>
-                <div class=".d-flex justify-content-end flex-fill">
-                <span class="badge rounded-pill bg-warning w-50 ">Débutant</span>
-                </div>
-                <p class="card-text">14/12/2021</p>
-                <p class="card-text">51100</p>
-                <p class="card-text">Tennis</p>
-                
-            </div>
-            </div>
-        </div>  
-        <div class=".d-flex justify-content-center m-3 "> 
-    <div class="card border-dark " style="width: 20rem;">
-        <img src="/images/foot.jpg"  class="card-img-top w-100" alt="imageSport">
-        <div class="card-body">
-            <h5 class="card-title">Tournoi de Tennis</h5>
-            <p class="card-subtitle">Procedente igitur mox tempore cum adventicium nihil inveniretur, relicta ora maritima . </p>
-            <div class=".d-flex justify-content-end flex-fill">
-            <span class="badge rounded-pill bg-warning w-50 ">Débutant</span>
-            </div>
-            <p class="card-text">14/12/2021</p>
-            <p class="card-text">51100</p>
-            <p class="card-text">Tennis</p>
-            
-        </div>
-        </div>
-    </div>  
-    <div class=".d-flex justify-content-center m-3 "> 
-        <div class="card border-dark " style="width: 20rem;">
-            <img src="/images/foot.jpg"  class="card-img-top w-100" alt="imageSport">
-            <div class="card-body">
-                <h5 class="card-title">Tournoi de Tennis</h5>
-                <p class="card-subtitle">Procedente igitur mox tempore cum adventicium nihil inveniretur, relicta ora maritima . </p>
-                <div class="d-flex justify-content-end flex-fill">
-                <span class="badge rounded-pill bg-warning w-50 ">Débutant</span>
-                </div>
-                <p class="card-text">14/12/2021</p>
-                <p class="card-text">51100</p>
-                <p class="card-text">Tennis</p>
-                
-            </div>
-            </div>
-        </div>  
-        <div class=".d-flex justify-content-center m-3 "> 
-        <div class="card border-dark " style="width: 20rem;">
-            <img src="/images/foot.jpg"  class="card-img-top w-100" alt="imageSport">
-            <div class="card-body">
-                <h5 class="card-title">Tournoi de Tennis</h5>
-                <p class="card-subtitle">Procedente igitur mox tempore cum adventicium nihil inveniretur, relicta ora maritima . </p>
-                <div class="d-flex justify-content-end flex-fill">
-                <span class="badge rounded-pill bg-warning w-50 ">Débutant</span>
-                </div>
-                <p class="card-text">14/12/2021</p>
-                <p class="card-text">51100</p>
-                <p class="card-text">Tennis</p>
-                
-            </div>
-            </div>
-        </div>
-        
-    
-    </div>
-</div>              
-
-  -->
